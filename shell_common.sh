b71ab5f50f00eec8b6c023e933e021d3b1e4fae7
Alex Klinkert <alex@klinkert.io>
2026-04-01 07:50:32 +0200

    fix: Update PATH
    Signed-off-by: Alex Klinkert <alex@klinkert.io>

--- shell_common.sh
+++ shell_common.sh
@@ -3,7 +3,7 @@
 # Use only POSIX-compatible syntax that works in both bash and zsh
 
 # PATH Configuration
-export PATH=~/.asdf/shims:~/bin:/usr/local/bin:$HOME/go/bin:$HOME/bin:$PATH:${HOME}/.krew/bin
+export PATH=~/.asdf/shims:~/.local/bin:~/bin:/usr/local/bin:$HOME/go/bin:$HOME/bin:$PATH:${HOME}/.krew/bin
 
 # Environment Variables
 export NODE_ENV=development
